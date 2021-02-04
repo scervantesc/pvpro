ba32ffc17d78fa864a53631161dc10cc2223d89c
Salvador A. Cervantes <scervantesc@live.com>
2021-02-04 17:12:43 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
-
+//actu
 
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
